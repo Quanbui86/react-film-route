ec2f4f244d5459d26315244da31f62e0307de5c3
Quanbui86 <136612066+Quanbui86@users.noreply.github.com>
2024-01-18 17:29:46 +0700

    17h30 18-01

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,16 +17,20 @@ export default function Header({ isScrolling }) {
     const goFoward = () => {
         navigate(+1)
     }
-    
+
     return (
         <section className={`z-index-5 pageSetup header-container ${isScrolling ? `position-fixed top-go-up-6rem` : 'position-fixed top-0'}`}>
             <div className={`header-top-bg ${isScrolling ? `` : ''}`}>
                 <div className={`container header-top-contain `}>
-                    <div className="logo"><Link to='/react-film-route' className={`${isActive => isActive ? '' : ''}`}><img src={pnglogo} alt='pnglogo' /></Link></div>
-                    <Search isScrolling={isScrolling}/>
+                    <div className="logo">
+                        <Link to='/react-film-route' className={`${isActive => isActive ? '' : ''}`}>
+                            <img src={pnglogo} alt='pnglogo' />
+                        </Link>
+                    </div>
+                    <Search isScrolling={isScrolling} />
                     <div className="user-icon">
-                        <NavLink to='/react-film-route/user' className={`fa-regular fa-user ${isActive => isActive ? 'active' : ''}`}></NavLink>
-                        <NavLink to='/react-film-route/favourite' className={`fa-regular fa-bookmark ${isActive => isActive ? 'active' : ''}`}></NavLink>
+                        <NavLink to='/react-film-route/user' className={`fa-regular fa-user not-active-class ${({ isActive }) => isActive ? "active" : ""}`}></NavLink>
+                        <NavLink to='/react-film-route/favourite' className={`fa-regular fa-bookmark not-active-class ${({ isActive }) => isActive ? "active" : ""}`}></NavLink>
                     </div>
                 </div>
             </div>
